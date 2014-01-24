eb4fb7f2c6952309ce3630262d82b2c291c3d802
Luanne Misquitta <luanne.coutinho@gmail.com>
2014-01-24 23:08:18 +0530

    Switch to GrapheneDB addon for neo4j

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -19,7 +19,6 @@ environments {
         grails {
             neo4j {
                 type = "rest"
-                //location = System.getenv('NEO4J_REST_URL') ?: "http://localhost:7474/db/data/"
                 location = System.getenv('GRAPHENEDB_URL') ?: "http://localhost:7474/db/data/"
             }
         }
