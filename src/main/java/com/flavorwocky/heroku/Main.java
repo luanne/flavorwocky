b0962237bca612de70767966ea09311f044bbf8c
Luanne Misquitta <luanne.coutinho@gmail.com>
2014-06-03 12:21:28 +0530

    Temp site

---
+++ src/main/java/com/flavorwocky/heroku/Main.java
@@ -0,0 +1,30 @@
+package com.flavorwocky.heroku;
+
+import org.apache.catalina.startup.Tomcat;
+
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        String webappDirLocation = "src/main/webapp/";
+        Tomcat tomcat = new Tomcat();
+
+        //The port that we should run on can be set into an environment variable
+        //Look for that variable and default to 8080 if it isn't there.
+        String webPort = System.getenv("PORT");
+        if (webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+
+        tomcat.setPort(Integer.valueOf(webPort));
+
+        tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
+
