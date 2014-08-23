37397c2c0ffaed2b6ba2a43309deeea08f6bc712
Luanne Misquitta <luanne.coutinho@gmail.com>
2014-08-23 16:09:49 +0530

    Search autocomplete and trio updates

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -3,15 +3,56 @@
  */
 
 (function () {
-    var app = angular.module('flavors', [ ]);
+    var app = angular.module('flavors', ['ui.bootstrap']);
 
-    app.controller('TrioController', ['$http',function($http) {
-    var flavors=this;
-    flavors.trios=[];
+app.factory("SearchService", ['$rootScope','$http',function($rootScope,$http) {
+  var currentIngredient=undefined;
+  var trios = [];
 
-    $http.get('/api/trios/i1').success(function(data) {
-        flavors.trios=data;
-    });
+  return {
+    getIngredient: function() {
+      return currentIngredient;
+    },
+    setIngredient : function(ing) {
+        currentIngredient = ing;
+        $http.get('/api/trios/' + ing).success(function(data) {
+              trios=data;
+              $rootScope.$broadcast('trios:updated',trios);
+           });
+
+    },
+    getTrios : function() {
+       return trios;
+    }
+  };
+}]);
+
+
+     app.controller('SearchController', ['$http', 'SearchService', function($http,SearchService)  {
+          var ingredientData = this;
+          ingredientData.ingredient=SearchService.getIngredient();
+          ingredientData.ingredients = [];
+          ingredientData.trios =[];
+
+          this.search =function($item, $model, $label) {
+                SearchService.setIngredient($item);
+           };
+
+          $http.get('/api/ingredients').success(function(data) {
+                     ingredientData.ingredients=data;
+                 });
+
+      }]);
+
+
+    app.controller('TrioController', ['$http', '$scope','SearchService',function($http,$scope,SearchService) {
+        var flavors=this;
+        flavors.trios=SearchService.getTrios();
+
+        $scope.$on('trios:updated', function(event,data) {
+         flavors.trios=data;
+       });
+
 
     }]);
 
